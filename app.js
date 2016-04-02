bbc963ddee702946420c9438848a5ef0254f30a1
puronglong <1240429156@qq.com>
2016-04-02 20:48:18 +0800

    add notation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//整个应用的后端的一个服务入口,执行它的话这个服务就会启动
 
 var express = require('express'),
   config = require('./config/config'),
@@ -15,10 +15,13 @@ var models = glob.sync(config.root + '/app/models/*.js');
 models.forEach(function (model) {
   require(model);
 });
+
+//生成express应用
 var app = express();
 
 require('./config/express')(app, config);
 
+//让这个express应用监听某个端口
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
