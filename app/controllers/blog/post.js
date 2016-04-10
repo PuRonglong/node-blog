d332b13bd7c2a10be2cab904b0bdcf8aff73cda6
puronglong <1240429156@qq.com>
2016-04-10 17:13:40 +0800

    add paging feature

--- app/controllers/blog/post.js
+++ app/controllers/blog/post.js
@@ -11,12 +11,26 @@ module.exports = function (app) {
 
 router.get('/', function (req, res, next) {
     Post.find({published: true})
+        .sort('created')
         .populate('authoer')
         .populate('category')
         .exec(function (err, posts) {
             if (err) return next(err);
-            res.render('blog/index', {//渲染的应是blog下面的index
-                posts: posts,
+
+            var pageNum = Math.abs(parseInt(req.query.page || 1, 10));//页数,让pageNum变成正整数
+            var pageSize = 10;//一页展示十条
+            var totalCount = posts.length;//得到文章总数
+            var pageCount = Math.ceil(totalCount / pageSize);//文章页数 = 文章总数 / 每页文章数
+
+            //如果当前页数大于总页数,把最后一页设为当前页数
+            if(pageNum > pageCount){
+                pageNum = pageCount;
+            }
+
+            res.render('blog/index', {
+                posts: posts.slice((pageNum - 1) * pageSize, pageNum * pageSize),
+                pageNum: pageNum,
+                pageCount: pageCount,
                 pretty: true
             });
     });
